5a27e8e8d37bc129cda9f941e05037460a845a62
nickg1only <g1.nikhil@gmail.com>
2021-01-06 19:01:06 -0500

    Separated Output from Script
    Created an Output Script file and a Script file.
    
    Output file contains a single statement that, when run, will source the Script file.
    
    Script file contains all the code that will be run, including the output statements.
    
    Also, I changed a few things in the script and added to my report.

---
+++ CYO_Adult_R_Output_Script.R
@@ -0,0 +1,3 @@
+#Run this statement to see output:
+
+source("CYO_Adult_R_Script.R", echo = FALSE)
--- CYO_Adult_R_Script.R
+++ CYO_Adult_R_Script.R
@@ -1,4 +1,4 @@
-options(echo = FALSE, verbose = FALSE, warn = FALSE)
+options(warn = -1)
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -47,43 +47,88 @@ train_y <- as.factor(train$income)
 test_x <- test[,-15]
 test_y <- as.factor(test$income)
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#Introduce the project to the user:
+
+
+cat("\n\n","Goal: Predict if income level is above 50K")
+cat("\n","Dataset: Adults in the 1994 Census")
+cat("\n\n","Features: ")
+cat(names(train_x), sep = "\n\t")
+cat("\n","Outcome: ","Income > 50K (Yes/No)")
+
+cat("\n\n\nPredictive Models: ","\n","\tDecision Tree","\n","\tRandom Forest","\n")
+
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Train the models.
 
 
+
 #Decision Tree:
 
 train_rpart <- train(train_x, train_y, method = "rpart")
-plot(train_rpart)
+rpart_best_cp <- train_rpart$bestTune
 
 rpart_preds <- predict(train_rpart, test_x)
 rpart_acc <- mean(rpart_preds == test_y)
 
 
-#Random Forest - type and enter "Y" when asked to run the following model,
-                # otherwise, hit enter or type something and hit enter):
-run_rf <- readline(prompt = "Run Random Forest? (WARNING: VERY SLOW)")
 
-if(run_rf == "Y"){
+#Random Forest - 
+# Enter "Y" when asked to run the following model,
+# otherwise, enter "N" or type anything and hit enter):
+
+run_rf <- readline(prompt = "Run Random Forest? (WARNING: VERY SLOW) Hit enter for Yes, type NO for No:  ")
+
+if(run_rf == ""){
   train_rf <- train(train_x, train_y, method = "rf") #VERY SLOW
-  plot(train_rf)
+  rf_best_cp <- train_rf$bestTune
   
   rf_preds <- predict(train_rf, test_x)
   rf_acc <- mean(rf_preds == test_y)
   
-  rm(train_rf, rf_preds)
 }
-
-rm(test_index, test_x, train_rpart, train_x, rf_preds, rpart_preds, test_y, train_y)
 
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #RESULTS:
 
+cat("\n\n-----\n\n","RESULTS: ","\n\n-------\n")
 
-cat("Accuracies:")
-cat("Decision Tree: ", rpart_acc)
-if(run_rf == "Y") cat("Random Forest: ", rf_acc)
+cat("Decision Tree: ","\n\n")
+cat("Best Complexity Parameter: ", as.numeric(rpart_best_cp), "\n")
+cat("Plot for Decision Tree: (see Plots)","\n\n")
+plot(train_rpart)
+cat("Accuracy: ", rpart_acc)
+
+cat("\n\n\n ----- \n\n")
+
+if(run_rf == ""){
+  cat("Random Forest: ","\n\n")
+  cat("Best Complexity Parameter: ", as.numeric(rf_best_cp), "\n")
+  cat("Plot for Random Forest: (see Plots)","\n\n")
+  plot(train_rf)
+  cat("Accuracy: ", rf_acc)
+}
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Remove unnecessary values:
+
+
+rm(test_index, test_x, train_x, test_y, train_y)
+
+rm(train_rpart, rpart_preds)
+
+if(run_rf == ""){
+  rm(train_rf, rf_preds)
+}
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+options(warn = 0)
